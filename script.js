2bc02d2764be525350ba9e1009e43d324943fb54
FranciscoReis77 <135717721+FranciscoReis77@users.noreply.github.com>
2023-06-06 17:56:24 +0100

    atualizacao1

---
+++ script.js
@@ -0,0 +1,13 @@
+const teste = document.getElementById("destaque");
+
+
+function show(){
+    if(teste.style.display == 'block'){
+        teste.style.display = 'none';
+    }
+    else{
+        teste.style.display = 'block';
+    }
+}
+
+console.log(teste);
